4e24c6cd0c3724499e7acba83a5b1a8dc5d9a0a2
Abhijit Gupta <avgupta456@gmail.com>
2020-09-11 23:15:01 -0400

    working gpt3

--- main.py
+++ main.py
@@ -1,9 +1,39 @@
 from flask import Flask
 
+import os
+from dotenv import load_dotenv, find_dotenv
+
+from gpt import GPT, Example, set_openai_key
+
+
 app = Flask(__name__)
+
+load_dotenv(find_dotenv(), override=True)
+set_openai_key(os.getenv("GPT_SECRET_KEY", ""))
+
+gpt = GPT(engine="davinci", temperature=0.5, max_tokens=100)
+
+
+gpt.add_example(Example("Two plus two equals four", "2 + 2 = 4"))
+gpt.add_example(Example("The integral from zero to infinity", "\\int_0^{\\infty}"))
+gpt.add_example(
+    Example(
+        "The gradient of x squared plus two times x with respect to x",
+        "\\nabla_x x^2 + 2x",
+    )
+)
+gpt.add_example(Example("The log of two times x", "\\log{2x}"))
+gpt.add_example(
+    Example("x squared plus y squared plus equals z squared", "x^2 + y^2 = z^2")
+)
 
 
 @app.route("/")
 def hello_world():
     return "Hello, World!"
 
+
+@app.route("/gpt3")
+def gpt3():
+    prompt = "integral from a to b of f of x"
+    return gpt.get_top_reply(prompt)
