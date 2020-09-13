709beb88c4e248e2f4caebb93ae681a58a8da0ef
shahjaidev <js5161@columbia.edu>
2020-09-12 22:33:52 -0400

    fixes, changes to summarizer + updated denial_letter sample

--- deeplearning_text_summarization/summarize_documents.py
+++ deeplearning_text_summarization/summarize_documents.py
@@ -2,14 +2,15 @@ from summarizer import Summarizer
 
 letterpath= "../resources/denial_letter.txt"
 
-def read_and_summarize(letterpath)
+def read_and_summarize(letterpath):
 	f= open("../resources/denial_letter.txt","r")
 	letter=f.read()
 	model = Summarizer()
-	#result1 = model(body, ratio=0.1)  # pass ratio
-	result2 = model(body, num_sentences=3)  # Will return 3 sentences 
+	#result1 = model(letter, ratio=0.1)  # pass ratio
+	result2 = model(letter, num_sentences=6)  # Will return 6 sentences 
 
-	print(result2)
+	print (result2)
+
 	return result2
 
 
